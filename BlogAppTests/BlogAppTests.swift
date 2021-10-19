b975e4b69e34ee74f5d30175f2da8d33891c14a3
yuanboz <yuanboz@appstate.edu>
2021-10-19 11:33:06 -0400

    Initial Commit

---
+++ BlogAppTests/BlogAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  BlogAppTests.swift
+//  BlogAppTests
+//
+//  Created by 周元博 on 10/19/21.
+//
+
+import XCTest
+@testable import BlogApp
+
+class BlogAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
